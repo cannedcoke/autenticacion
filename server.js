b63308ba9419f030f237fc421629d96b350548e9
Cyanide <miqueas736d@gmail.com>
2026-02-24 09:01:51 -0300

    feat: setup JSON and cookie parsing middleware

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require("express")
 
 const app = express()
 
+const routes = require("./routes/router")
+
+const path = require("path")
+
+const cookieParser = require("cookie-parser");
 
 app.use(express.json())
 
-const path = require("path")
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "views")));
 
@@ -16,9 +21,10 @@ app.use("/css", express.static(path.join(__dirname, "static/css")));
 // Serve JS files
 app.use("/js", express.static(path.join(__dirname, "static", "js")));
 
-const routes = require("./routes/router")
-
 app.use("/logging",routes)
+
+app.use("/dashboard",routes)
+
 
 
 app.listen(5000, ()=>{
