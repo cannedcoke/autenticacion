b81bb0d380825685cd4919c8bebb49fa32a07b26
Cyanide <miqueas736d@gmail.com>
2026-02-24 08:59:32 -0300

    feat: adding dashboard route functionalities

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const authenticate = require("../middlewares/authenticate");
 const controller = require("../controllers/controller");
-
 
 // defino api endpoints y le indico que funcion del controlador usar
 
 router.post("/signUp", controller.signUp);
+
 router.post("/logIn", controller.logIn);
+
+router.post("/logout", controller.logout);
+
+router.get("/getData",authenticate, controller.displayDash);
+
+router.delete("/delete",authenticate, controller.delRow);
 
 // exporto para poder acceder externamente
 module.exports = router;
