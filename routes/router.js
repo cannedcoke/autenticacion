b8d252c5da09ed210c3761f45561375a9f775d20
Cyanide <miqueas736d@gmail.com>
2026-02-25 11:06:27 -0300

    feat: adding middleware to avoid retries

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
+const retires = require("../middlewares/retires");
 const controller = require("../controllers/controller");
 
 // defino api endpoints y le indico que funcion del controlador usar
 
 router.post("/signUp", controller.signUp);
 
-router.post("/logIn", controller.logIn);
+router.post("/logIn",retires, controller.logIn);
 
 router.post("/logout", controller.logout);
 
